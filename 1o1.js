636e8a192c01bc19832f9243138d5a68e82879ed
mcubd <106980362+mcubd@users.noreply.github.com>
2024-01-24 20:52:12 +0600

    Update 1o1.js

--- 1o1.js
+++ 1o1.js
@@ -61,7 +61,7 @@ order.setAttribute('onclick', 'order()')
 contact.innerText = 'Movies!'
 // contact.innerText = ''
 
-h4.innerText = ' '
+h4.innerText = 'sohan amar number e call de taratari '
 button1.innerText = 'Marvel'
 button2.innerText = 'Marvel-Seris'
 button3.innerText = 'fox & sony'
