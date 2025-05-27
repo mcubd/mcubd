81a284d165b3d8be2853b4c7ab9f92efaec79ecf
mcubd <106980362+mcubd@users.noreply.github.com>
2025-05-28 05:00:35 +0600

    Create key2.js

---
+++ key2.js
@@ -0,0 +1,96 @@
+window.active_input66 = "one";
+window.inn = async function (e) {
+    let mm66;
+    if (window.active_input66 == "one") {
+        mm66 = window.mathField;
+    } else if (window.active_input66 == "two") {
+        mm66 = mathField6;
+    }
+
+
+    mm66.focus();
+
+    console.log(e);
+    if (e == "a^2") {
+
+        mm66.write(`^2`);
+    } else if (e == "a^b") {
+        mm66.write(`^{}`);
+    } else if (e == "sqrt{x}") {
+        mm66.write(`\\sqrt{{}}`);
+    } else if (e == "frac{d}{dx}") {
+        mm66.write(`\\frac{d}{dx}`);
+    } else if (e == "lim_{x\\to a}") {
+        mm66.write(`\\lim_{x\\to a}`);
+    } else if (e == "frac{a}{b}") {
+        mm66.write(`\\frac{}{}`);
+    } else if (e == "sin") {
+        mm66.write(`sin`);
+    } else if (e == "cos") {
+        mm66.write(`cos`);
+    } else if (e == "tan") {
+        mm66.write(`tan`);
+    } else if (e == "sin1") {
+        mm66.write(`sin^{-1}`);
+    } else if (e == "cos1") {
+        mm66.write(`cos^{-1}`);
+    } else if (e == "tan1") {
+        mm66.write(`tan^{-1}`);
+    } else if (e == "alpha") {
+        mm66.write(`\\alpha`);
+    } else if (e == "beta") {
+        mm66.write(`\\beta`);
+    } else if (e == "gamma") {
+        mm66.write(`\\gamma`);
+    } else if (e == "delta") {
+        mm66.write(`\\delta`);
+    } else if (e == "theta") {
+        mm66.write(`\\theta`);
+    } else if (e == "sigma") {
+        mm66.write(`\\sigma`);
+    } else if (e == "omega") {
+        mm66.write(`\\omega`);
+    } else if (e == "infty") {
+        mm66.write(`\\infty`);
+    } else if (e == "approx") {
+        mm66.write(`\\approx`);
+    } else if (e == "ne") {
+        mm66.write(`\\ne`);
+    } else if (e == "phi") {
+        mm66.write(`\\phi`);
+    } else if (e == "perp") {
+        mm66.write(`\\perp`);
+    } else if (e == "sqrt[n]{x}") {
+        mm66.write(`\\sqrt[{}]{{}}`);
+    } else if (e == "int_a^b") {
+        mm66.write(`\\int_{}^{}`);
+    } else if (e == "pi") {
+        mm66.write(`\\pi`);
+    } else if (e == "λ") {
+        mm66.write(`λ`);
+    }else if (e == "î") {
+        mm66.write(`î`);
+    }else if (e == "ĵ") {
+        mm66.write(`ĵ`);
+    }else if (e == "k̂") {
+        mm66.write(`k̂`);
+    }else if (e == "vec{A}") {
+        mm66.write(`\\vec{A}`);
+    }else if (e == "mathbf{A}") {
+        mm66.write(`\\mathbf{A}`);
+    }else if (e == "mathbf{vec{B}}") {
+        mm66.write(`\\mathbf{\\vec{B}}`);
+    }else if (e == "fff") {
+        mm66.write(`fff`);
+    }else if (e == "fff") {
+        mm66.write(`fff`);
+    }
+    
+
+
+
+    mm66.focus();
+
+}
+
+
