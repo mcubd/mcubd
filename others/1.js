a7ebe9167e4ce575c14e37b1e634a34c17e6d8af
mcubd <106980362+mcubd@users.noreply.github.com>
2026-01-15 17:08:17 +0600

    Update 1.js

--- others/1.js
+++ others/1.js
@@ -2795,10 +2795,12 @@ setTimeout(() => {
 
 
  
-
  
 // seris('Weak [500MB]','weakk',[],'500MB')
 
+
+seris('The Lunchbox [330MB]','lunch',["https://www.googleapis.com/drive/v3/files/157XRZwYobUxjDOFjIiTnzOiCUA_L68Hq?alt=media&key=AIzaSyCZ8IJ6yrMTPetNZyYVH4vyX4rc0L46AGc"],'330MB')
+ 
  seris('হাওয়া [300MB]','hawaa',["https://www.googleapis.com/drive/v3/files/1XwlzlfejQAaw5r4E5erxhIiTowdLJ4Lp?alt=media&key=AIzaSyCZ8IJ6yrMTPetNZyYVH4vyX4rc0L46AGc"],'300MB','mv')
  seris('little miss chaos  [600MB]','limica',["https://www.googleapis.com/drive/v3/files/1VEX2h7_HQD_b7h6Jafz3cgtJppZKQHHG?alt=media&key=AIzaSyCZ8IJ6yrMTPetNZyYVH4vyX4rc0L46AGc"],'600MB','mv')
 seris('অসময় [1.4GB]','osomo',["https://www.googleapis.com/drive/v3/files/1NVf6s1fAhqheHrLzy3fjll1Qes6Ni2tj?alt=media&key=AIzaSyCZ8IJ6yrMTPetNZyYVH4vyX4rc0L46AGc"],'1.4GB','mv')
